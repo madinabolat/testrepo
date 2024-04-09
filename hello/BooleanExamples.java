4fc6366f637204df1f1392e9669a26bfc563f885
madinabolat <madinabolat01@gmail.com>
2024-04-09 19:24:10 -0400

    java examples

---
+++ hello/BooleanExamples.java
@@ -0,0 +1,14 @@
+public class BooleanExamples {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println(!a);
+        System.out.println(a&&a);
+        System.out.println(a||a);
+        int x = 10;
+        int y = 12;
+        boolean b = x > y;
+        System.out.println(b);
+        boolean c = x == y;
+        System.out.println(c);
+    }
+}
