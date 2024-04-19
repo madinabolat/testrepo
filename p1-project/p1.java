87fb2a81de5126988fec251da9ab1602b2709ac8
madinabolat <madinabolat01@gmail.com>
2024-04-18 21:48:56 -0400

    config: udemy, p1 start

---
+++ p1-project/p1.java
@@ -0,0 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class p1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Hello World");
+        FileReader input = new FileReader("input.txt");
+    }
+}
