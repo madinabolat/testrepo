525775a3d345fa3805cb2309521f46a90943a940
madinabolat <madinabolat01@gmail.com>
2024-04-29 21:16:32 -0400

    config

---
+++ csp-hw-w6/MatrixMult.py
@@ -0,0 +1,15 @@
+pip3 install memory-profiler
+
+a = [[1, 2],
+    [3, 4]]
+b = [[5, 6],
+    [7, 8]]
+c = [[0 for x in range(2)] for y in range(2)] 
+for i in range(2):
+    for j in range(2):
+        c[i][j]=0
+        for k in range(2):
+            c[i][j]=c[i][j]+a[i][k]*b[k][j]
+print(c)
+
+     
