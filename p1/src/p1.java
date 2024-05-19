e280c52823f1ba45c95cf0343529c8b6403bd840
madinabolat <madinabolat01@gmail.com>
2024-05-18 23:26:48 -0400

    config: p1

--- p1/src/p1.java
+++ p1/src/p1.java
@@ -1,6 +1,17 @@
-public class p1 {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
-    public static void main(String[] args) {
+public class p1 {
+    public static void main(String[] args) throws Exception{
+        //1 read file
+        File inputDirectory = new File("/Users/madinabolat/Documents/GitHub/testrepo/p1/input.txt");
+        FileReader inputData = new FileReader(inputDirectory);
+        BufferedReader inputText = new BufferedReader(inputData);
+        String inputAsString = inputText.readLine();
+        System.out.println(inputAsString);
+        //2 identify all words in a text, assign a code to each
+
 
     }
 }
