d9788c685822df1c443dd7cdd18ed06155d1b91a
madinabolat <madinabolat01@gmail.com>
2024-05-21 22:17:17 -0400

    Update Main.java

--- mentorship-2/src/Main.java
+++ mentorship-2/src/Main.java
@@ -3,9 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         int[][] gameBoard = new int[3][3];
+        int counter = 1;
         for (int i=0;i<3;i++){
             for (int j=0;j<3;j++){
-                gameBoard[i][j] = i+j;
+                gameBoard[i][j] = counter;
+                counter ++;
             }
         }
         for (int i=0;i<3;i++){
