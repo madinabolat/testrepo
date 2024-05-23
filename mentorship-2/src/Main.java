6997ea4a99e178cb42f6d9d30fa5263f3548a32e
madinabolat <madinabolat01@gmail.com>
2024-05-22 23:13:31 -0400

    Update Main.java

--- mentorship-2/src/Main.java
+++ mentorship-2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -16,5 +18,10 @@ public class Main {
             }
             System.out.println();
         }
+        Scanner scannerInput = new Scanner(System.in);
+        System.out.println("enter user Input");
+        String userInput = scannerInput.nextLine();
+        System.out.println("the user inputted: "+userInput);
+        scannerInput.close();
     }
 }
