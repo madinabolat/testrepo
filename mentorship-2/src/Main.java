f8baf879376953bf30e5ac9a244a50ad1d3bf8d6
madinabolat <madinabolat01@gmail.com>
2024-06-10 22:40:32 -0400

    Update Main.java

--- mentorship-2/src/Main.java
+++ mentorship-2/src/Main.java
@@ -48,22 +48,6 @@ public class Main {
     public static void main(String[] args) {
         String[][] gameBoard = createEmptyBoard(3);
         printBoard(gameBoard);
-        //placeElementHere(gameBoard, "x", 0,1);
-        //placeElementHere(gameBoard, "o", 0,2);
-        //String position1 = takeUserInput();
-        //int i1 = Integer.parseInt(String.valueOf(position1.charAt(0)))-1;
-        //int j1 = Integer.parseInt(String.valueOf(position1.charAt(2)))-1;
-        //placeElementHere(gameBoard,"x",i1,j1);
-
-        //String position2 = takeUserInput();
-        //int i2 = Integer.parseInt(String.valueOf(position2.charAt(0)))-1;
-        //int j2 = Integer.parseInt(String.valueOf(position2.charAt(2)))-1;
-        //placeElementHere(gameBoard,"o",i2,j2);
-
-        //considerations: there is already element there, out of bounds, row / column number invalid
-
-
-
         int n = 3; // board n x n
         int maxStepsFirstUser = n * n / 2 + 1;
         int maxStepsSecondUser = n * n - maxStepsFirstUser;
